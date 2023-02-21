f5f3c1b49f8530a1848d47784a28cf3c2d204ec1
rajesh9938 <110902554+rajesh9938@users.noreply.github.com>
2023-02-22 00:24:11 +0530

    Update helpers.py

--- staf/helpers.py
+++ staf/helpers.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 from django.template.loader import get_template
-import xhtml2pdf.pisa as pisa
+# import xhtml2pdf.pisa as pisa
 import uuid
 from django.conf import settings
 
